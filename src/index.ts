359437e82261f9af75a13eecb68848f68b198cba
99-Knots <ninety9knots@outlook.com>
2024-05-29 15:37:14 +0200

    added skybox

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import * as BABYLON from '@babylonjs/core'
+import * as BABYLON from '@babylonjs/core'; 
+import { SkyMaterial } from '@babylonjs/materials'
 
 const canvas = document.createElement('canvas');
 canvas.id = "render-canvas";
@@ -9,8 +10,15 @@ document.body.appendChild(canvas);
 const engine = new BABYLON.Engine(canvas, true);
 const scene = new BABYLON.Scene(engine);
 
-const camera = new BABYLON.ArcRotateCamera('camera1', 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+const camera = new BABYLON.ArcRotateCamera('camera1', 0.5, 0.9, 10, BABYLON.Vector3.Zero(), scene);
 camera.attachControl(canvas, true);
+
+const skyMaterial = new SkyMaterial("skyMaterial", scene);
+skyMaterial.backFaceCulling = false;
+skyMaterial.inclination = 0.3;
+
+const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
+skybox.material = skyMaterial;
 
 const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), scene);
 
